6d03c5db00d86d40859f969166f545cbb111cb89
roshangaddam <roshangaddam139@gmail.com>
2024-10-25 16:46:14 +0530

    msg

---
+++ day2.js
@@ -0,0 +1,51 @@
+let a=+prompt("enter value");
+let b=+prompt("enter value");
+console.log("arithmetic operators");
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a++);
+console.log(a);
+console.log(++a);
+console.log(a--);
+console.log(a);
+console.log(--a);
+console.log(b**3);
+console.log("assignment operators");
+console.log(a+=5);
+console.log(a/=5);
+console.log(a*=5);
+console.log("comparison operators");
+console.log(a==b);
+console.log(a===b);
+console.log(a<b);
+console.log(a>b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(a!=b);
+console.log(a!==b);
+console.log("logical operators");
+console.log("true&&true",a>5 && b>5);
+console.log("true&&false",a>5 && b>100);
+console.log("false&&true",b>100 && a>5);
+console.log("false&&false",b>100 && b>100);
+console.log("true||true",a>5||b>5);
+console.log("true||false",a>5||b>100);
+console.log("false||true",b>100||a>5);
+console.log("false||false",b>100||b>100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
